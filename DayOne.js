5b1286622579b6188593e0f43c25cfb17874a565
KARTIKPAUL <arpilar401@gmail.com>
2024-07-14 20:13:40 +0530

    This is 30 Days JavaScript Challange in Chai Aur Code

---
+++ DayOne.js
@@ -0,0 +1,51 @@
+//Variables And Data Types
+
+//Task / Activities
+
+//Activity 1 
+
+var num = 10;
+console.log(num);
+
+let s = "Chai"
+console.log(s);
+
+//Activity 2
+
+const value = false;
+console.log(value);
+
+
+//Activity 3
+
+let a = 10;
+let b = "Code";
+let val = false;
+let obj = {
+    name: "Kartik",
+    age: 20
+}
+let arr = [1,2,3,4,5]
+
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof val);
+console.log(typeof obj);
+console.log(typeof arr);
+
+//Activity 4
+
+let firstValue = 100;
+console.log(firstValue);
+firstValue = 200;
+console.log(firstValue);
+
+const initialValue = 500;
+console.log(initialValue);
+//initialValue = 1000;
+//console.log(initialValue);
+
+
+
+
+
