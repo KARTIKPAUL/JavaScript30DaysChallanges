b321266eb21a7427d75dc07e608475587af903d0
KARTIKPAUL <arpilar401@gmail.com>
2024-07-14 21:57:40 +0530

    Day Two Completed

---
+++ DayTwo.js
@@ -0,0 +1,91 @@
+//Day 2
+
+//Activity 1
+
+//task 1
+//Add Two Numbers
+let a = 10;
+let b = 10;
+console.log(`Addition is ${a+b}`);
+
+//task 2
+//Subtract Two Numbers
+let c = 20;
+let d = 10;
+console.log(`Subtraction is ${c-d}`);
+
+//task 3
+//Multiply Two Numbers
+let e = 10;
+let f = 10;
+console.log(`Multiplication is ${e*f}`);
+
+//task 4
+//Divide Two Numbers
+let g = 20;
+let h = 10;
+console.log(`Divide is ${g/h}`);
+
+//task 5
+//Remainder of Two Numbers
+let i = 10;
+let j = 10;
+console.log(`Remainder is ${i%j}`);
+
+//Activity 2
+//task 6
+let initialValue = 10;
+initialValue += 10;
+console.log(`Initial Value is : ${initialValue}`);
+
+//task 7
+let value = 20;
+value -= 10;
+console.log(`value is : ${value}`);
+
+//Activity 3
+//task 8
+let firstValue = 10;
+let secondValue = 20;
+if(firstValue > secondValue){
+    console.log(`${firstValue} is greater than ${secondValue}`);
+}else if(firstValue < secondValue){
+    console.log(`${firstValue} is less than ${secondValue}`);
+}
+
+//task 9
+if(firstValue >= secondValue){
+    console.log(`${firstValue} is greater than or equal to ${secondValue}`);
+}else if(firstValue <= secondValue){
+    console.log(`${firstValue} is less than or equal to ${secondValue}`);
+}
+
+//task 10
+console.log(firstValue == secondValue);
+console.log(firstValue === secondValue);
+
+//Activity 4
+
+//task 11
+if(firstValue > 5 && secondValue > 10){
+    console.log('Both Condition are true');
+}
+
+//task 12
+
+if(firstValue > 10 || secondValue > 10){
+    console.log('One Or Both Condition are true');
+}
+
+//task 13
+if(firstValue != secondValue){
+    console.log(`${firstValue} and ${secondValue} are Not Equal`);
+}
+
+//task 14 => Ternary Operator
+
+let num = 10;
+num > 0 ? console.log(`${num} is Positive`) : console.log(`${num} is Negetive`); 
+
+
+
