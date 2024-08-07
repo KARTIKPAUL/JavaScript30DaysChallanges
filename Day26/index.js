b4024dd5758fb958b1511a1f020453b6023af3df
KARTIKPAUL <arpilar401@gmail.com>
2024-08-07 14:16:10 +0530

    Day 26 Completed

---
+++ Day26/index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+const {Server} = require("socket.io")
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+//Socket.io
+io.on('connection',(socket) => {
+    socket.on('userMessage' , (message) => {
+        //console.log(`A new User Message ${message}`);
+        io.emit('message',message);
+    });
+})
+
+const PORT = 3000;
+app.use(express.static(path.resolve("./public")));
+app.get('/',(req,res) => {
+    return res.send('/public/index.html')
+})
+server.listen(PORT,() => {
+    console.log(`Server Started At http://localhost${PORT}`);
+})
